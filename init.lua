72254761707d4dd744e9870c751476e62d1d23b6
jlcoulter <104843199+jlcoulter@users.noreply.github.com>
2026-01-21 06:24:50 +1100

    Configure Vim settings in init.lua
    Set up basic Vim options and leader keys.

--- init.lua
+++ init.lua
@@ -1,3 +1,22 @@
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.clipboard = "unnamedplus"
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
