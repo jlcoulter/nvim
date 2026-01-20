9f63647077b45eb85ee2ccd50c19b15065d997ab
jlcoulter <coulter.l.jack@gmail.com>
2026-01-21 09:24:45 +1100

    Wed 21 Jan 2026 09:24:45 AEDT

--- init.lua
+++ init.lua
@@ -303,7 +303,44 @@ require("lazy").setup({
             return { timeout_ms = 500 }
           end,
         },
-      }
+      },
+      {
+        "mfussenegger/nvim-dap",
+        recommended = true,
+        desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
+
+        dependencies = {
+          "rcarriga/nvim-dap-ui",
+          -- virtual text for the debugger
+          {
+            "theHamsta/nvim-dap-virtual-text",
+            opts = {},
+          },
+        },
+      },
+      {
+        "jay-babu/mason-nvim-dap.nvim",
+        dependencies = "mason.nvim",
+        cmd = { "DapInstall", "DapUninstall" },
+        opts = {
+          -- Makes a best effort to setup the various debuggers with
+          -- reasonable debug configurations
+          automatic_installation = true,
+
+          -- You can provide additional configuration to the handlers,
+          -- see mason-nvim-dap README for more information
+          handlers = {},
+
+          -- You'll need to check that you have the required things installed
+          -- online, please don't ask me how to install them :)
+          ensure_installed = {
+            -- Update this to ensure that you have the debuggers for the langs you want
+          },
+        },
+        -- mason-nvim-dap is loaded when nvim-dap loads
+        config = function() end,
+      },
+
 
 
     }
