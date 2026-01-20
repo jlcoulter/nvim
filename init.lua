b8af20f364d58fa46e5f6a1aa62c67927df1ac24
jlcoulter <104843199+jlcoulter@users.noreply.github.com>
2026-01-21 06:23:11 +1100

    Add telescope.nvim plugin configuration

--- init.lua
+++ init.lua
@@ -25,13 +25,25 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- add your plugins here
-        {
-    "folke/tokyonight.nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("tokyonight")
-    end,
-  },
+    {
+      "folke/tokyonight.nvim",
+      priority = 1000,
+      config = function()
+        vim.cmd.colorscheme("tokyonight")
+      end,
+    },
+    {
+      "nvim-telescope/telescope.nvim",
+      tag = "0.1.6",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      config = function()
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>ff", builtin.find_files)
+        vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+        vim.keymap.set("n", "<leader>fb", builtin.buffers)
+      end,
+     },
+
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
