86b6e372ef9c3e9240b36b1e4a94e6b385fcd420
jlcoulter <104843199+jlcoulter@users.noreply.github.com>
2026-01-21 06:37:48 +1100

    Configure nvim-treesitter plugin
    Added nvim-treesitter plugin configuration with auto-install and syntax highlighting options.

--- init.lua
+++ init.lua
@@ -59,5 +59,23 @@ require("lazy").setup({
         vim.keymap.set("n", "<leader>fb", builtin.buffers)
       end,
      },
+    }, 
+    {
+      "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate", -- Command to run after installation/update
+  opts = {
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query" }, -- Languages to auto-install
+    auto_install = true, -- Automatically installs missing parsers
+    highlight = {
+      enable = true, -- Enables syntax highlighting
+    },
+    indent = {
+      enable = true, -- Enables indentation
+    },
+  },
+  config = function(_, opts)
+    -- This function runs after the plugin is loaded
+    require("nvim-treesitter.configs").setup(opts)
+  end,
     }
 })
