c848f789307173f8e658546b079f8abf39e59295
jlcoulter <coulter.l.jack@gmail.com>
2026-01-21 07:20:27 +1100

    Wed 21 Jan 2026 07:20:27 AEDT

--- init.lua
+++ init.lua
@@ -79,36 +79,6 @@ require("lazy").setup({
     -- this is equivalent to setup({}) function
 },
 {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-      local cmp = require("cmp")
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "buffer" },
-          { name = "path" },
-        },
-      })
-    end,
-  },
-{
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
@@ -130,7 +100,7 @@ require("lazy").setup({
     dependencies = { "mason.nvim" },
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "pyright", "bashls" },
+        ensure_installed = { "luaformatter", "rubyfmt", "lua_ls", "pyright", "bashls", "json-lsp", "java-language-server" },
       })
     end,
   },
