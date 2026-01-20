f1c8b05f0392a2fa199f6090bbfeedfec7dcdfbe
jlcoulter <coulter.l.jack@gmail.com>
2026-01-21 07:01:21 +1100

    Wed 21 Jan 2026 07:01:21 AEDT

--- init.lua
+++ init.lua
@@ -63,7 +63,7 @@ require("lazy").setup({
       "nvim-treesitter/nvim-treesitter",
       build = ":TSUpdate",
       opts = {
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python"},
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "java", "ruby", "json" },
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
