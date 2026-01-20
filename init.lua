8da6410e8c966ef0ca76ef1cfcb898d8a5f5faec
jlcoulter <coulter.l.jack@gmail.com>
2026-01-21 08:00:23 +1100

    Wed 21 Jan 2026 08:00:23 AEDT

--- init.lua
+++ init.lua
@@ -278,27 +278,6 @@ require("lazy").setup({
       desc = "Format Injected Langs",
     },
   },
-  init = function()
-    -- Install the conform formatter on VeryLazy
-    LazyVim.on_very_lazy(function()
-      LazyVim.format.register({
-        name = "conform.nvim",
-        priority = 100,
-        primary = true,
-        format = function(buf)
-          require("conform").format({ bufnr = buf })
-        end,
-        sources = function(buf)
-          local ret = require("conform").list_formatters(buf)
-          ---@param v conform.FormatterInfo
-          return vim.tbl_map(function(v)
-            return v.name
-          end, ret)
-        end,
-      })
-    end)
-  end,
-    ---@type conform.setupOpts
     local opts = {
       default_format_opts = {
         timeout_ms = 3000,
